4a8e66c41e2694403c81ed781544734042505e3b
Travis Martin <trvsmartin@gmail.com>
2023-03-16 14:58:44 -0700

    setup profile data interface

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -4,13 +4,27 @@ import { ProfileHeader } from "../../components/ProfileHeader/ProfileHeader";
 import { ProfileAbout } from "../../components/ProfileAbout/ProfileAbout";
 import { Affiliations } from "../../components/Affiliations/Affiliations";
 import { ContentPosts } from "../../components/ContentPosts/ContentPosts";
+import ProfileData from "../../data/UserProfile.json";
 
+interface ProfileProps {
+  first_name: string;
+  last_name: string;
+  headline: string;
+  location: {
+    city: string;
+    state_province: string;
+    country: string;
+  };
+  profile_picture: string;
+  profile_banner: string;
+  about: string;
+}
 export const UserProfile: React.FC = () => {
   return (
     <>
       <AppNavbar />
       <div className="profile">
-        <ProfileHeader />
+        <ProfileHeader profileData={ProfileData} />
         <ProfileAbout />
         <Affiliations />
         <ContentPosts />
