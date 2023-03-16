5d7182745203f8bc0dd2a404f9001dfdc090873a
Ian DC <jokerzamba@hotmail.com>
2023-03-15 18:47:58 -0700

    Create basic layout of AppNavbar.tsx for mobile

--- src/pages/Network/Network.tsx
+++ src/pages/Network/Network.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import AppNavbar from "../../components/AppNavbar/AppNavbar";
 
 const Network = () => {
-  return <div>Network</div>;
+  return (
+    <>
+      <AppNavbar />
+    </>
+  );
 };
 
 export default Network;
