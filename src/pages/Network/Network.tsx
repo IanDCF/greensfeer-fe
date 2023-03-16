1fecf5440f8c7f746a6887df080c9d34c00fc3da
Ian DC <jokerzamba@hotmail.com>
2023-03-15 19:15:18 -0700

    Create basic document layout for Network.tsx

--- src/pages/Network/Network.tsx
+++ src/pages/Network/Network.tsx
@@ -1,9 +1,21 @@
 import AppNavbar from "../../components/AppNavbar/AppNavbar";
-
+import "./Network.scss";
 const Network = () => {
   return (
     <>
       <AppNavbar />
+      <div className="network">
+        <div className="network__contacts">
+          <div className="network__title">Requests</div>
+          <div className="network__pending">Pending Request</div>
+          <div className="network__connections">Connection</div>
+        </div>
+
+        <div className="network__recommended">
+          <div className="network__title">Recommended</div>
+          <div className="network__recomend-box">Recommended Contact</div>
+        </div>
+      </div>
     </>
   );
 };
