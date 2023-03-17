aa257cd1b842628977de60ee1cedece5e5047aea
Miguel Lopez Pedres <lopezpedres@gmail.com>
2023-03-16 22:04:55 -0600

    example: Using Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthProvider, { useAuth } from "./context/AuthProvider/AuthProvider";
 import Landing from "./pages/Landing/Landing";
 import NotFound from "./pages/NotFound/NotFound";
-import Register from "./pages/Register/Register";
-import RegisterInfo from "./pages/Register/RegisterInfo";
+import Register from "./pages/RegisterPage/RegisterPage";
 import Subscribe from "./pages/Subscribe/Subscribe";
 
 const App = () => {
+  const { currentUser } = useAuth();
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="*" element={<NotFound />} />
         <Route path="/subscribe" element={<Subscribe />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/register/info" element={<RegisterInfo />} />
+        <Route
+          path="/youarebiutiful"
+          element={
+            <div style={{ fontSize: "15rem" }}>
+              You are biutifullll {"<3"}
+              <br />
+              {currentUser?.email} with id: {currentUser?.uid}
+            </div>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
