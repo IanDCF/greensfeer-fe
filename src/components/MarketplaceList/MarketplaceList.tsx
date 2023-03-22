f5470e8b212b8f2bba80c85a10cce9394e632d66
travis <trvsmartin@gmail.com>
2023-03-22 11:54:41 -0700

    render market post cards

--- src/components/MarketplaceList/MarketplaceList.tsx
+++ src/components/MarketplaceList/MarketplaceList.tsx
@@ -1,15 +1,20 @@
+import { IMarketPost } from "customTypes";
 import MarketplaceCard from "../MarketplaceCard/MarketplaceCard";
 import "./MarketplaceList.scss";
+interface MarketProps {
+  Posts?: IMarketPost[];
+}
 
-const MarketplaceList = () => {
+const MarketplaceList: React.FC<MarketProps> = ({ Posts }) => {
   return (
     <section className="marketplace-list">
-      <MarketplaceCard />
-      <MarketplaceCard />
-      <MarketplaceCard />
-      <MarketplaceCard />
-      <MarketplaceCard />
-      <MarketplaceCard />
+      {Posts
+        ? Posts[0]
+          ? Posts.map((post) => (
+              <MarketplaceCard key={post.market_post_id} Post={post} />
+            ))
+          : "small loading"
+        : "loading"}
     </section>
   );
 };
--- src/pages/Marketplace/Marketplace.tsx
+++ src/pages/Marketplace/Marketplace.tsx
@@ -2,10 +2,21 @@ import { useLocation, matchPath } from "react-router-dom";
 import "./Marketplace.scss";
 import MarketplaceList from "../../components/MarketplaceList/MarketplaceList";
 import MarketplaceSelected from "../../components/MarketplaceSelected/MarketplaceSelected";
+import allMarketPosts from "../../helpers/allMarketFetcher";
+import { useState, useEffect } from "react";
+import { IMarketPost } from "customTypes";
 
 const Marketplace: React.FC = () => {
+  const [marketPosts, setMarketPosts] = useState<IMarketPost[]>([]);
   const location = useLocation();
   const path = location.pathname;
+  useEffect(() => {
+    const getData = async () => {
+      const posts = await allMarketPosts();
+      setMarketPosts(posts);
+    };
+    getData();
+  }, []);
 
   const marketplaceListMatch = matchPath(path, "/marketplace");
 
@@ -17,7 +28,7 @@ const Marketplace: React.FC = () => {
   return (
     <section className="marketplace-container">
       {marketplaceItemSelectedMatch && <MarketplaceSelected />}
-      {marketplaceListMatch && <MarketplaceList />}
+      {marketplaceListMatch && <MarketplaceList Posts={marketPosts} />}
     </section>
   );
 };
