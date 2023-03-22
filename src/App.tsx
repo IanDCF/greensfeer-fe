cf117d252c840fb3a00c159cc04eea9b152a70b6
Miguel Lopez Pedres <lopezpedres@gmail.com>
2023-03-21 21:42:04 -0600

    refactor: added id to company route

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const App = () => {
         <Route path="/messages" element={<Messages />} />
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/messages/chat" element={<ChatRoom />} />
-        <Route path="/company" element={<CompanyProfile />} />
+        <Route path="/company/:companyId" element={<CompanyProfile />} />
       </Routes>
       <Navigation />
     </BrowserRouter>
