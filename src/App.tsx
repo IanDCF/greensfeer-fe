e692a567dc8e401eb34446835ab208d522241c15
Ian DC <jokerzamba@hotmail.com>
2023-03-22 16:41:35 -0700

    Create CreateCompany.tsx page + add basic layout and styling to Form1

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Subscribe from "./pages/Subscribe/Subscribe";
 import { UserProfile } from "./pages/UserProfile/UserProfile";
 import { CompanyProfile } from "./pages/CompanyProfile/CompanyProfile";
 import Marketplace from "./pages/Marketplace/Marketplace";
+import CreateCompany from "./pages/CreateCompany/CreateCompany";
 
 const App = () => {
   const { currentUser } = useAuth();
@@ -42,6 +43,8 @@ const App = () => {
         <Route path="/company" element={<CompanyProfile />} />
         <Route path="/marketplace" element={<Marketplace />} />
         <Route path="/marketplace/item" element={<Marketplace />} />
+        <Route path="/create-company/step1" element={<CreateCompany />} />
+        <Route path="/create-company/step2" element={<CreateCompany />} />
       </Routes>
       <Navigation />
     </BrowserRouter>
---
+++ src/pages/CreateCompany/CompanyForm2.tsx
@@ -0,0 +1,7 @@
+import "./CreateCompany.scss";
+
+const CompanyForm2 = () => {
+  return <div>CompanyForm2</div>;
+};
+
+export default CompanyForm2;
