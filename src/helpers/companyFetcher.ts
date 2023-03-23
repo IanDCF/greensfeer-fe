8dc5f281a0bdad6a91b5e49453c37e866f9656aa
Miguel Lopez Pedres <lopezpedres@gmail.com>
2023-03-22 18:29:03 -0600

    refactor:return null if status error

--- src/helpers/companyFetcher.ts
+++ src/helpers/companyFetcher.ts
@@ -4,11 +4,13 @@ import { ICompany } from "customTypes";
 const getCompany = async (companyId: string) => {
   const URL_BASE = import.meta.env.VITE_REACT_APP_BASE_URL;
   try {
-    const { data } = await axios.get(`${URL_BASE}/company/${companyId}`);
+    const { data, status } = await axios.get(
+      `${URL_BASE}/company/${companyId}`
+    );
+    if (status !== 200) return null;
     return data as ICompany;
   } catch (error) {
     console.log(error);
-    throw new Error();
   }
 };
 
