de32ae68952a050657bfb2f57c045cfb8e26dab4
Miguel Lopez Pedres <lopezpedres@gmail.com>
2023-03-22 18:30:08 -0600

    refactor: redirect client if no company

--- src/pages/CompanyProfile/CompanyProfile.tsx
+++ src/pages/CompanyProfile/CompanyProfile.tsx
@@ -1,20 +1,19 @@
 import { ProfileHeader } from "../../components/ProfileHeader/ProfileHeader";
 import { ProfileAbout } from "../../components/ProfileAbout/ProfileAbout";
 import { MarketListings } from "../../components/MarketListings/MarketListings";
-import CompanyData from "../../data/CompanyProfile.json";
-import MarketPosts from "../../data/MarketListings.json";
 import { useEffect, useState } from "react";
 import "../UserProfile/UserProfile.scss";
 import getMarketPost from "../../helpers/marketPostFetcher";
 import { ICompany, IMarketPost } from "customTypes";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import getCompany from "../../helpers/companyFetcher";
 
 export const CompanyProfile: React.FC = () => {
   const { companyId } = useParams();
+  const navigate = useNavigate();
   const [products, setProducts] = useState<IMarketPost[]>([]);
   const [services, setServices] = useState<IMarketPost[]>([]);
-  const [company, setCompany] = useState<ICompany>();
+  const [company, setCompany] = useState<ICompany | null>(null);
   useEffect(() => {
     const getData = async () => {
       if (companyId) {
@@ -22,8 +21,12 @@ export const CompanyProfile: React.FC = () => {
         if (companyData) {
           setCompany(companyData);
           const marketPost = await getMarketPost(companyId);
-          setProducts(marketPost.filter((post) => post.p));
-          setServices(marketPost.filter((post) => !post.p));
+          if (marketPost) {
+            setProducts(marketPost.filter((post) => post.p));
+            setServices(marketPost.filter((post) => !post.p));
+          }
+        } else {
+          navigate("/home");
         }
       }
     };
@@ -32,13 +35,15 @@ export const CompanyProfile: React.FC = () => {
 
   return (
     <>
-      <div className="user-profile-container">
-        <ProfileHeader CompanyData={company} user={false} />
-        <ProfileAbout CompanyData={company} user={false} />
-        {/* <Affiliations /> */}
-        <MarketListings posts={products} title={"Products"} />
-        <MarketListings posts={services} title={"Services"} />
-      </div>
+      {company && (
+        <div className="user-profile-container">
+          <ProfileHeader CompanyData={company} user={false} />
+          <ProfileAbout CompanyData={company} user={false} />
+          {/* <Affiliations /> */}
+          <MarketListings posts={products} title={"Products"} />
+          <MarketListings posts={services} title={"Services"} />
+        </div>
+      )}
     </>
   );
 };
