8d93a920ce37104d42db3886c85ac0eee7a87f0a
Ian DC <jokerzamba@hotmail.com>
2023-04-04 08:56:14 -0700

    Add signout functionality with auth.signOut()

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -1,13 +1,26 @@
 import "./UserMenu.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../../firebase/firebase";
 
 const UserMenu = () => {
+  const navigate = useNavigate();
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+      navigate("/");
+      console.log("User has been signed out");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="user-menu">
       <Link to="/profile" className="user-menu__item">
         My Profile
       </Link>
-      <div className="user-menu__item">Sign Out</div>
+      <div className="user-menu__item" onClick={handleSignOut}>
+        Sign Out
+      </div>
     </div>
   );
 };
