2b667f5ec23dc6356a128f6800f9327509327fbe
travis <trvsmartin@gmail.com>
2023-05-03 15:32:54 -0700

    feat: add company search to filter
    <body>
    
    Issue #

--- src/components/MarketFilterMenu/MarketFilterMenu.tsx
+++ src/components/MarketFilterMenu/MarketFilterMenu.tsx
@@ -1,6 +1,9 @@
-import { ReactElement, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { BsDot } from "react-icons/bs";
 import "./MarketFilterMenu.scss";
 import { ICompany } from "customTypes";
+import getAllCompanies from "../../helpers/allCompanyFetcher";
 
 const sectors: string[] = [
   "Various Sectors",
@@ -29,17 +32,103 @@ const sectors: string[] = [
   "Other",
 ];
 
+const projtypes: string[] = [
+  "Agroforestry",
+  "Biomass Energy Generation",
+  "Blue Carbon",
+  "Carbon Capture & Storage",
+  "Forest Conservation & Management",
+  "Landfill Gas Capture & Utilization",
+  "Methane Capture & Utilization",
+  "REDD+",
+  "Reforestation & Afforestation",
+  "Soil Conservation & Erosion Control",
+  "Sustainable Agriculture & Farming",
+  "Renewable Energy Installations",
+  "Program Types:",
+  "Agricultural Soil Carbon Sequestration",
+  "Afforestation & Reforestation",
+  "Building Energy Efficiency",
+  "Carbon Offset Programs",
+  "Energy Efficiency",
+  "Fuel Switching",
+  "Green Transportation",
+  "Landfill Gas-to-Energy",
+  "Livestock Methane Reduction",
+  "Renewable Energy",
+  "Waste-to-Energy",
+];
+
+const servtype: string[] = [
+  "API Provider",
+  "Broker",
+  "Carbon Offset Consulting",
+  "Credit Rating Provider",
+  "Exchange",
+  "Financing & Sponsorship",
+  "Governance & Accreditation",
+  "Insurance",
+  "Market Research & Data Platform",
+  "Marketplace",
+  "Measuring, Reporting, & Verification",
+  "Project Developers & Aggregator",
+  "Software as a Service",
+  "Third-Party Auditor",
+  "Verification & Validation Body",
+];
+
 const MarketFilterMenu = (): ReactElement => {
   const changeHandler = (e: React.FormEvent) => {
     const radio = e.currentTarget as HTMLInputElement;
     setListingType(radio.value);
   };
-  const [companies, setCompanies] = useState<ICompany[]>();
-  const [compSearch, setCompSearch] = useState<String>();
-  const [selectedCompanies, setSelectedCompanies] = useState<ICompany[]>();
+  const [companies, setCompanies] = useState<ICompany[]>([]);
+  const [compSearch, setCompSearch] = useState<string>("");
+  const [searchDropdown, setSearchDropdown] = useState(false);
+  const [compResult, setCompResult] = useState<ICompany[]>([]);
+  const searchResultLength = compResult?.length || 0;
+  const getCom = async () => {
+    const companies = await getAllCompanies();
+    setCompanies(companies);
+  };
+  useEffect(() => {
+    getCom();
+  }, []);
 
-  const [locationSearch, setLocationSearch] = useState<String[]>([]);
-  const [selectedLocations, setSelectedLocations] = useState<String[]>();
+  const handleSearch = () => {
+    toggleSearchDropdown();
+    setCompSearch("");
+  };
+
+  const toggleSearchDropdown = () => {
+    setSearchDropdown(!searchDropdown);
+  };
+
+  useEffect(() => {
+    if (compSearch.length > 0) {
+      setSearchDropdown(true);
+      setCompResult(
+        companies?.filter((company) => {
+          const regex = new RegExp(`${compSearch}`, "i");
+          const match = (nameStr: string | undefined) => {
+            if (nameStr?.match(regex)) return true;
+          };
+          if (
+            "company_name" in company &&
+            match(company.company_name)
+            // first name match || last name match || name match
+          )
+            return true;
+        })
+      );
+    } else {
+      setSearchDropdown(false);
+    }
+  }, [compSearch]);
+  //   const [selectedCompanies, setSelectedCompanies] = useState<ICompany[]>();
+
+  //   const [locationSearch, setLocationSearch] = useState<String[]>([]);
+  //   const [selectedLocations, setSelectedLocations] = useState<String[]>();
 
   const [listingType, setListingType] = useState<string>("");
   return (
@@ -47,7 +136,6 @@ const MarketFilterMenu = (): ReactElement => {
       <form action="submit">
         <fieldset>
           <legend>Post Type:</legend>
-          {/* TODO: add clickhandler that displays proj/service type on radio change */}
           <label htmlFor="Project">
             Project
             <input
@@ -71,16 +159,29 @@ const MarketFilterMenu = (): ReactElement => {
         </fieldset>
         {listingType ? (
           listingType === "Project" ? (
-            <label htmlFor="type">
-              <select id="type" defaultValue={"Type"}>
-                <option defaultValue={undefined} disabled>
-                  Type
-                </option>
-                <option value="Broker">Broker</option>
-              </select>
-            </label>
+            <fieldset>
+              <legend>Project types:</legend>
+              {projtypes.map((proj) => {
+                return (
+                  <label key={proj}>
+                    {proj}
+                    <input type="checkbox" name={proj} />
+                  </label>
+                );
+              })}
+            </fieldset>
           ) : (
-            "Service list"
+            <fieldset>
+              <legend>Service types:</legend>
+              {servtype.map((serv) => {
+                return (
+                  <label key={serv}>
+                    {serv}
+                    <input type="checkbox" name={serv} />
+                  </label>
+                );
+              })}
+            </fieldset>
           )
         ) : (
           <div>When filtering by Project/Service, refine by type here</div>
@@ -98,11 +199,49 @@ const MarketFilterMenu = (): ReactElement => {
         </fieldset>
         <fieldset>
           <legend>Search Companies:</legend>
-          <input type="text" />
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => {
+              setCompSearch(e.target.value);
+            }}
+            value={compSearch}
+          />
           {/* get all companies, set search input to state, filter companies */}
         </fieldset>
+        {searchDropdown && searchResultLength > 0 && (
+          <div className="search__dropdown" onClick={handleSearch}>
+            {compResult?.map((profile: ICompany) => {
+              return (
+                <div
+                  key={profile.company_id}
+                  className="search__link"
+                >
+                  {profile.logo ? (
+                    <img className="search__photo" src={`${profile.logo}`} />
+                  ) : (
+                    <div className="search__photo">
+                      <FaUserCircle />
+                    </div>
+                  )}
+                  <div className="search__text">
+                    {<div className="search__name">{profile.company_name}</div>}
+                  </div>
+                  <div className="search__separator">
+                    <BsDot />
+                  </div>
+                  <div className="search__headline">Company</div>
+                  <div className="search__separator">
+                    <BsDot />
+                  </div>
+                  <div className="search__headline">{profile?.sector}</div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </form>
-      <form
+      {/* <form
         action="submit"
         onSubmit={(e) => {
           e.preventDefault();
@@ -124,7 +263,7 @@ const MarketFilterMenu = (): ReactElement => {
               })
             : ""}
         </fieldset>
-      </form>
+      </form> */}
     </div>
   );
 };
