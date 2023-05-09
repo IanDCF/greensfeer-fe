7aa1e12f3642fa91438909d33304f8c1c6565cff
travis <trvsmartin@gmail.com>
2023-05-09 11:51:51 -0700

    feat:implement protected routes

---
+++ src/components/Protected/Protected.tsx
@@ -0,0 +1,17 @@
+import { User } from "firebase/auth";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthProvider/AuthProvider";
+interface Props{
+    children:JSX.Element
+    currentUser:User|null
+}
+
+const Protected= ({ children }:Props):JSX.Element => {
+    const {currentUser}=useAuth()
+  if (!currentUser) {
+    return <Navigate to="/" replace />;
+  }
+  return <>{children}</>;
+};
+
+export default Protected;
