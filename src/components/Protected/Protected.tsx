f6dba7a26c4ed2e70ecd5c18daa8bf85ca5bb549
travis <trvsmartin@gmail.com>
2023-05-11 10:34:39 -0700

    fix: correct spacing

--- src/components/Protected/Protected.tsx
+++ src/components/Protected/Protected.tsx
@@ -1,13 +1,13 @@
 import { User } from "firebase/auth";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthProvider/AuthProvider";
-interface Props{
-    children:JSX.Element
-    currentUser:User|null
+interface Props {
+  children: JSX.Element;
+  currentUser: User | null;
 }
 
-const Protected= ({ children }:Props):JSX.Element => {
-    const {currentUser}=useAuth()
+const Protected = ({ children }: Props): JSX.Element => {
+  const { currentUser } = useAuth();
   if (!currentUser) {
     return <Navigate to="/" replace />;
   }
